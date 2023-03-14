4400cb29ae06be31113880acd4f888c3548bc320
Michael Rich <richmr2174@gmail.com>
2023-03-14 16:07:43 -0700

    prototype secrets files

---
+++ appsecrets_proto.py
@@ -0,0 +1,11 @@
+# Fill out the app secrets below and remove the "_proto" from the filename
+
+# These keys much match the ones in appsecrets.js
+admin_site_key = "foo"
+user_site_key = "bar"
+
+# set to "prod" to eliminate the API Docs standard to FastAPI
+app_mode = "test"
+
+# Must be valid sqlAConnectionString
+sqlAConnectionString = "sqlite://..."
---
+++ static/js/appsecrets_proto.js
@@ -0,0 +1,5 @@
+// Fill in the secrets below and remove "_proto" from filename
+// The secrets much match the ones in appsecrets.py
+
+var admin_site_key = "foo"; 
+var user_site_key = "bar";
