509d81c5b635f47a859ceb47dab1f1f940b0eebd
Michael Rich <richmr2174@gmail.com>
2023-03-14 16:08:09 -0700

    spelling fix

--- static/js/appsecrets_proto.js
+++ static/js/appsecrets_proto.js
@@ -1,5 +1,5 @@
 // Fill in the secrets below and remove "_proto" from filename
-// The secrets much match the ones in appsecrets.py
+// The secrets must match the ones in appsecrets.py
 
 var admin_site_key = "foo"; 
 var user_site_key = "bar";
